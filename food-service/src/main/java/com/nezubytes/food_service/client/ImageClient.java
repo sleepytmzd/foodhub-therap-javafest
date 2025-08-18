8d375bb6233ca978af28415fd9cbd7e4f02043bb
sleepytmzd <depressionstonks420@gmail.com>
2025-08-18 13:38:35 +0600

    k8s done

--- food-service/src/main/java/com/nezubytes/food_service/client/ImageClient.java
+++ food-service/src/main/java/com/nezubytes/food_service/client/ImageClient.java
@@ -2,6 +2,7 @@ package com.nezubytes.food_service.client;
 
 import com.nezubytes.food_service.dto.UploadResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name="image-service", url = "${IMAGE_SERVICE_URL}", path="/api/image")
+@FeignClient(name="image-service", url = "${image.service.url}", path="/api/image")
 public interface ImageClient {
     @PostMapping(path="/upload", consumes=MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<UploadResponseDTO> upload(@RequestPart("file") MultipartFile file);
--- image-service-sb/src/main/java/com/nezubytes/image_service_sb/Config/CloudinaryConfiguration.java
+++ image-service-sb/src/main/java/com/nezubytes/image_service_sb/Config/CloudinaryConfiguration.java
@@ -1,16 +1,26 @@
 package com.nezubytes.image_service_sb.Config;
 
 import com.cloudinary.Cloudinary;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CloudinaryConfiguration {
+//    private final Environment environment;
+//
+//    public CloudinaryConfiguration(Environment environment) {
+//        this.environment = environment;
+//    }
+@Value("${cloudinary.url}")
+private String cloudinaryUrl;
     @Bean
     public Cloudinary cloudinary(){
-        Dotenv dotenv = Dotenv.load();
-        Cloudinary cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+//        Dotenv dotenv = Dotenv.load();
+//        Cloudinary cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+//        Cloudinary cloudinary = new Cloudinary(environment.getProperty("cloudinary.url"));
+        Cloudinary cloudinary = new Cloudinary(cloudinaryUrl);
         System.out.println("Connected to cloudinary!\n" + cloudinary.config.cloudName);
         return cloudinary;
     }
